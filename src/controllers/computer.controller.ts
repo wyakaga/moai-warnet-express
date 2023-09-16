dce769140e0a8efd8f7c7de9960cea98398bdc4c
wyakaga <123617005+wyakaga@users.noreply.github.com>
2023-09-16 16:56:46 +0700

    [FEAT] Create readAll computer router and controller

--- src/controllers/computer.controller.ts
+++ src/controllers/computer.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { Prisma } from "@prisma/client";
 
 import prisma from "../configs/prisma.config";
 import { response } from "../utils/response";
@@ -27,4 +28,54 @@ const create = async (req: Request, res: Response) => {
 	}
 };
 
-export default { create };
+const readAll = async (req: Request, res: Response) => {
+	try {
+		let order: Prisma.ComputerOrderByWithRelationInput = { id: "asc" };
+
+		if (req.query.order === "asc") {
+			order = { computerNumber: "asc" };
+		}
+
+		if (req.query.order === "desc") {
+			order = { computerNumber: "desc" };
+		}
+		const group = String(req.query.group).toLowerCase() === "true" ? true : false;
+		const limit = Number(req.query.limit) || 10;
+		const page = Number(req.query.page) || 1;
+		const skip = page === 1 ? 0 : (page - 1) * limit;
+
+		const result = await prisma.computer.findMany({
+			where: {
+				isUsed: group,
+			},
+			orderBy: order,
+			take: limit,
+			skip,
+		});
+
+		const count = await prisma.computer.count({
+			where: {
+				isUsed: group,
+			},
+		});
+
+		const totalPages = Math.ceil(count / limit);
+		const nextPage = page < totalPages ? page + 1 : null;
+		const prevPage = page > 1 ? page - 1 : null;
+
+		const meta = {
+			totalItems: count,
+			totalPages,
+			currentPage: page,
+			nextPage: nextPage ? `${req.protocol}://${req.headers.host}?page=${nextPage}` : null,
+			prevPage: prevPage ? `${req.protocol}://${req.headers.host}?page=${prevPage}` : null,
+		};
+
+		response(res, 200, "OK", { result, meta });
+	} catch (error) {
+		console.log(error);
+		response(res, 500, "Internal Server Error", (error as Error).message);
+	}
+};
+
+export default { create, readAll };
--- src/routers/computer.router.ts
+++ src/routers/computer.router.ts
@@ -5,5 +5,6 @@ import computerController from "../controllers/computer.controller";
 const computerRouter: Router = Router();
 
 computerRouter.post("/", computerController.create);
+computerRouter.get("/", computerController.readAll);
 
 export default computerRouter;
