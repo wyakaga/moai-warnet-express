40b58564d3ef08bcaece68c764b99516fca57ebf
wyakaga <123617005+wyakaga@users.noreply.github.com>
2023-09-16 14:35:14 +0700

    [FEAT] Create readAll user router and countroller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { Prisma } from "@prisma/client";
 
 import prisma from "../configs/prisma.config";
 import { response } from "../utils/response";
@@ -31,4 +32,61 @@ const create = async (req: Request, res: Response) => {
 	}
 };
 
-export default { create };
+const readAll = async (req: Request, res: Response) => {
+	try {
+		let order: Prisma.UserOrderByWithRelationInput = { username: "asc" };
+
+		if (req.query.order === "poor") {
+			order = { balance: "asc" };
+		}
+
+		if (req.query.order === "rich") {
+			order = { balance: "desc" };
+		}
+
+		const search = String(req.query.search) || "";
+		const limit = Number(req.query.limit) || 10;
+		const page = Number(req.query.page) || 1;
+		const skip = page === 1 ? 0 : (page - 1) * limit;
+
+		const result = await prisma.user.findMany({
+			where: {
+				username: {
+					contains: search,
+					mode: "insensitive",
+				},
+			},
+			orderBy: order,
+			take: limit,
+			skip,
+		});
+
+		const count = await prisma.user.count({
+			where: {
+				username: {
+					contains: search,
+					mode: "insensitive",
+				},
+			},
+		});
+
+		const totalPages = Math.ceil(count / limit);
+		const nextPage = page < totalPages ? page + 1 : null;
+		const prevPage = page > 1 ? page - 1 : null;
+
+		const meta = {
+			totalItems: count,
+			totalPages,
+			currentPage: page,
+			nextPage: nextPage ? `${req.protocol}://${req.headers.host}?page=${nextPage}` : null,
+			prevPage: prevPage ? `${req.protocol}://${req.headers.host}?page=${prevPage}` : null,
+		};
+
+		response(res, 200, "OK", { result, meta });
+	} catch (error) {
+		console.log(error);
+		response(res, 500, "Internal Server Error", (error as Error).message);
+	}
+};
+
+export default { create, readAll };
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -5,5 +5,6 @@ import userController from "../controllers/user.controller";
 const userRouter: Router = Router();
 
 userRouter.post("/", userController.create);
+userRouter.get("/", userController.readAll);
 
 export default userRouter;
