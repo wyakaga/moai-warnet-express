0bab50ea9aecee81ee3bb3a12a60cb7e72766dc4
wyakaga <123617005+wyakaga@users.noreply.github.com>
2023-09-16 18:32:02 +0700

    [FEAT] Add cors and helmet

--- index.ts
+++ index.ts
@@ -1,15 +1,17 @@
 import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
+import helmet from "helmet";
 
 import mainRouter from "./src/routers";
 
 const app = express();
 const PORT = 8080;
 
-app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms")
-);
+app.use(cors());
+app.use(helmet());
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,5 +19,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(mainRouter);
 
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at port ${PORT}`);
+	console.log(`⚡️[server]: Server is running at port ${PORT}`);
 });
